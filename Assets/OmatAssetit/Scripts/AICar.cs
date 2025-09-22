d789ae311dfc35283bea5391110074e1653fb3b4
DESKTOP-TEM8N3F\Mutti8888 <taavi.makela@gmail.com>
2025-09-22 18:19:34 +0300

    Ai auton liikkumista pistetty käyntiin

--- Assets/OmatAssetit/Scripts/AICar.cs
+++ Assets/OmatAssetit/Scripts/AICar.cs
@@ -3,16 +3,15 @@ using UnityEngine;
 public class AICar : MonoBehaviour
 {
     public Transform[] waypoints;
-    
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    private int currentWaypointIndex = 0;
 
     // Update is called once per frame
     void Update()
     {
-        
+        Transform target = waypoints[currentWaypointIndex];
+
+       
+        transform.Translate(Vector3.forward);
+
     }
 }
